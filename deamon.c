da7987eea0db33e5725660495ea61aa9b1993058
luisxmetalx <you@example.com>
2017-08-24 15:27:37 -0500

    practica casi terminada

--- deamon.c
+++ deamon.c
@@ -1,27 +1,60 @@
-
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
 #include <sys/stat.h>
-#include <fcntl.h>
-#include <sys/wait.h>
-#include <signal.h>
-void deamon()
+#include <string.h>
+int main(int argc, char* argv[])
 {
-    if (( pid = fork()) < 0) {
-       perror("fork error");
-    } else if (pid == 0) { // child 
-        umask(0);
-    } else {
-        //Atrapamos la señal en el padre
-        signal(SIGTSTP,catch);
-} 
-int main()
-{
-    return 0;    
+    FILE *fp= NULL;
+    FILE *cpu= NULL;
+    char *buf= (char*) malloc(sizeof(char)*1000);
+    pid_t process_id = 0;
+    pid_t sid = 0;
+    // Create child process
+    process_id = fork();
+    // Indication of fork() failure
+    if (process_id < 0)
+    {
+        printf("fork failed!\n");
+        // Return failure in exit status
+        exit(1);
+    }
+    // PARENT PROCESS. Need to kill it.
+    if (process_id > 0)
+    {
+        printf("process_id of child process %d \n", process_id);
+        // return success in exit status
+        exit(0);
+    }
+    //unmask the file mode
+    umask(0);
+    //set new session
+    sid = setsid();
+    if(sid < 0)
+    {
+        // Return failure
+        exit(1);
+    }
+    // Change the current working directory to root.
+    chdir("/");
+    // Close stdin. stdout and stderr
+    close(STDIN_FILENO);
+    close(STDOUT_FILENO);
+    close(STDERR_FILENO);
+    // Open a log file in write mode.
+    fp = fopen ("log_cpu.log", "w+");
+    cpu = popen("top -bn2 | grep '%Cpu' | tail -1", "r");
+    while (1)
+    {
+        //Dont block context switches, let the process sleep for some time
+        sleep(1);
+        fread(buf,100,1, cpu);
+        printf("buffer: %s", buf);
+        fprintf(fp, "Logging info...\n");
+        fflush(fp);
+        // Implement and call some function that does core work for this daemon.
+    }
+    fclose(fp);
+    return (0);
 }
